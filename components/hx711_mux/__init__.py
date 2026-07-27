08da6eb01ec18cc74573d9d517e1430207d08292
hugogithubs <142120946+hugogithubs@users.noreply.github.com>
2026-07-27 23:54:10 +0200

    Update __init__.py

--- components/hx711_mux/__init__.py
+++ components/hx711_mux/__init__.py
@@ -1,22 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import gpio
-from esphome.const import CONF_ID, CONF_CLK_PIN, CONF_DOUT_PIN
+from esphome import pins
+from esphome.const import CONF_ID, CONF_CLK_PIN
 
-# Eindeutiger Namespace
+# C++ Namespace und Hub-Klasse deklarieren
 hx711_mux_ns = cg.esphome_ns.namespace("hx711_mux")
 HX711MuxHub = hx711_mux_ns.class_("HX711MuxHub", cg.Component)
+
+# ERZWINGUNGS-TRICK: Sagt dem ESPHome-Builder, dass dieses Projekt zwingend 
+# das button- und sensor-Framework einkompilieren muss!
+DEPENDENCIES = ["sensor", "button"]
+
+CONF_DOUT_PIN = "dout_pin"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(HX711MuxHub),
-        cv.Required(CONF_CLK_PIN): gpio.gpio_output_pin_schema,
-        cv.Required(CONF_DOUT_PIN): gpio.gpio_input_pin_schema,
+        cv.Required(CONF_CLK_PIN): pins.gpio_output_pin_schema,
+        cv.Required(CONF_DOUT_PIN): pins.gpio_input_pin_schema,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_PComponent(HX711MuxHub, config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
     clk = await cg.gpio_pin_expression(config[CONF_CLK_PIN])
